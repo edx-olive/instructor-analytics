07d0eda2176c368107a54d36e214d45c491b5ba0
Arsen Turchanikov <arsen.turchanikov@raccoongang.com>
2019-10-15 11:50:16 +0000

    [SGA] Add filtering for insights tab

--- rg_instructor_analytics/static/rg_instructor_analytics/js/insights-tab.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/insights-tab.js
@@ -2,6 +2,8 @@ function InsightsTab(button, content) {
     'use strict';
     var InsightsTab = new Tab(button, content);
     var $loader = $('#insights-loader');
+    var sitesSelector = InsightsTab.content.find('#insight-site-selector');
+    var sortableColumns = $("#insights-content").find(".sortable");
 
     function toggleLoader() {
         $loader.toggleClass('hidden');
@@ -11,15 +13,46 @@ function InsightsTab(button, content) {
         alert("Can't load data!");
     }
 
+    sitesSelector.off();
+
+    sitesSelector.on('change', function () {
+        InsightsTab.loadTabData(1, null, null, $(this).children("option:selected").val());
+    });
+
+    function sortColumns(currentPage) {
+        return function () {
+            var el = $(this),
+                sortKey = el.data('value'),
+                selectedMicrosite = sitesSelector.children("option:selected").val();
+
+            if (!el.hasClass("ascending") && !el.hasClass("descending")) {
+                el.addClass("ascending");
+            } else {
+                el.toggleClass("ascending descending");
+            }
+
+            var ordering = el.hasClass("descending") ? "DESC" : "ASC";
+
+            sortableColumns.not(el).removeClass("ascending descending");
+
+            InsightsTab.loadTabData(currentPage, sortKey, ordering, selectedMicrosite);
+        }
+    }
+
     function onInsightsLoad(response) {
         var analyticsContent = InsightsTab.content.find('#insights-tbody'),
             pagination = InsightsTab.content.find('#insights-pagination');
 
         pagination[0].innerHTML = renderPagination(response.count_pages, response.current_page);
 
+        if (response.microsites_names.length) {
+            $('.sites-selector-wrapper').removeClass('hidden');
+            sitesSelector[0].innerHTML = renderSitesSelector(response.microsites_names, response.ms_selected);
+        }
+
         $(".page-link").on("click", function (e) {
             e.preventDefault();
-            InsightsTab.loadTabData(this.dataset.value);
+            InsightsTab.loadTabData(this.dataset.value, null, null, response.ms_selected);
         });
 
         if (response.courses.length > 0) {
@@ -30,6 +63,10 @@ function InsightsTab(button, content) {
             $("#current-enrollment").html(totalMetrics.total_current_enrolled);
             $("#week-change").html(totalMetrics.total_diff_enrolled);
             $("#passed-enrollment").html(totalMetrics.certificates);
+
+            sortableColumns.off();
+
+            sortableColumns.on("click", sortColumns(response.current_page));
 
         } else {
             analyticsContent[0].innerHTML = '<div>' + django.gettext("No available statistics.") + '</div>';
@@ -54,6 +91,14 @@ function InsightsTab(button, content) {
             `
     }
 
+    function renderSitesSelector(sites, selected) {
+        let selector = "<option>All courses</option>";
+        for (let i = 0; i < sites.length; i++) {
+            selector += `<option ${sites[i] === selected ? 'selected' : ''}>${sites[i]}</option>`
+        }
+        return selector;
+    }
+
     function renderPagination(count, current) {
         var pagination = '';
         if (current === 1) {
@@ -73,11 +118,15 @@ function InsightsTab(button, content) {
         return pagination
     }
 
-    InsightsTab.loadTabData = function (page) {
+    InsightsTab.loadTabData = function (page, sortKey, ordering, microsite) {
         $.ajax({
             type: 'POST',
-            url: 'api/insights/?page=' + (page || 1),  // TODO(arsentur) remove hardcode
-            data: {},
+            url: 'api/insights/' + '?page=' + (page || 1),  // TODO(arsentur) remove hardcode
+            data: {
+                'microsite': microsite,
+                'sortKey': (sortKey || 'course'),
+                'ordering': (ordering || 'ASC'),
+            },
             dataType: 'json',
             traditional: true,
             success: onInsightsLoad,
--- rg_instructor_analytics/views/insights.py
+++ rg_instructor_analytics/views/insights.py
@@ -6,14 +6,14 @@ import json
 from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Max, Min, Q
 from django.http import JsonResponse
-from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 
 from lms.djangoapps.certificates.models import GeneratedCertificate
-from openedx.features.course_experience import course_home_url_name
+from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
 from rg_instructor_analytics.utils.decorators import instructor_access_required_without_course
 from rg_instructor_analytics_log_collector.models import EnrollmentByDay
 
@@ -71,50 +71,26 @@ class InsightsStatisticView(View):
         return total_metrics
 
     @staticmethod
-    def generate_query(limit, offset):
+    def get_microsite_filter(microsites, ms_selected):
         """
-        Helper method that generates raw query for sql request.
+        Helper method that returns course_org_filter for selected microsite.
 
-
-        :param limit: (int) Query limit
-        :param offset: (int) Query offset
-        :return: (str) sql query for the request.
+        :return: string or tuple with course_org_filter value(s) for filtering
+                 or None if SiteConfiguration does not exist.
         """
 
-        return """
-            SELECT 
-            enrollment_by_day.id,
-            enrollment_by_day.course,
-            MIN(enrollment_by_day.enrolled)             AS diff_enr_min,
-            MAX(enrollment_by_day.enrolled)             AS diff_enr_max,
-            MAX(enrollment_by_day.total)                AS total,
-            MAX(enrollment_by_day.enrolled)             AS enrolled_max,
-            course_overview.start                       AS start,
-            course_overview.end                         AS end,
-            course_overview.lowest_passing_grade        AS lowest_passing_grade,
-            generated_certificates.certificates_count   AS cert_count,
-            SUM(
-                CASE WHEN 
-                    grade_statistic.total > course_overview.lowest_passing_grade 
-                THEN 1 
-                ELSE 0 END
-                ) AS total_passing
-            FROM rg_instructor_analytics_log_collector_enrollmentbyday AS enrollment_by_day
-                LEFT JOIN course_overviews_courseoverview AS course_overview
-                    ON enrollment_by_day.course = course_overview.id
-                LEFT JOIN (
-                    SELECT generated_certificates.course_id,
-                        COUNT(generated_certificates.id) AS certificates_count
-                    FROM certificates_generatedcertificate AS generated_certificates
-                    GROUP BY generated_certificates.id
-                    ) generated_certificates
-                    ON enrollment_by_day.course =  generated_certificates.course_id
-                LEFT JOIN rg_instructor_analytics_gradestatistic AS grade_statistic
-                    ON enrollment_by_day.course = grade_statistic.course_id
-            GROUP BY enrollment_by_day.course
-            ORDER BY enrollment_by_day.course ASC
-            LIMIT {limit} OFFSET {offset};
-        """.format(limit=limit, offset=offset)
+        try:
+            m_site = microsites.get(site__name=ms_selected)
+            course_org_filter = m_site.values.get('course_org_filter')
+
+            if course_org_filter is not None:
+                if isinstance(course_org_filter, list):
+                    course_org_filter = tuple(str(course_filter) for course_filter in course_org_filter)
+
+                return course_org_filter
+
+        except ObjectDoesNotExist:
+            return
 
     def post(self, request, **kwargs):
         """
@@ -127,34 +103,41 @@ class InsightsStatisticView(View):
 
         page = int(request.GET.get('page', 1)) - 1
 
-        courses = EnrollmentByDay.objects.raw(
-            self.generate_query(COUNT_PER_PAGE * page + COUNT_PER_PAGE, page * COUNT_PER_PAGE)
+        # Columns sorting parameters
+        sort_key = request.POST.get('sortKey', 'course')
+        ordering = request.POST.get('ordering', 'ASC')
+
+        # Microsites filtering parameters
+        ms_selected = request.POST.get('microsite')
+
+        microsites = SiteConfiguration.objects.all().select_related('site')
+
+        if ms_selected:
+            microsite_filter = self.get_microsite_filter(microsites, ms_selected)
+        else:
+            microsite_filter = None
+
+        courses = EnrollmentByDay.objects.extract_analytics_data(
+            COUNT_PER_PAGE * page + COUNT_PER_PAGE,
+            page * COUNT_PER_PAGE,
+            sort_key,
+            ordering,
+            microsite_filter
         )
 
-        output = []
+        count_courses = EnrollmentByDay.objects.get_courses_count_by_site(microsite_filter)
 
-        for course in courses:
-            output.append(
-                {
-                    "name": str(course.course),
-                    "course_url": reverse(course_home_url_name(course.course), args=[unicode(course.course)]),
-                    "total": int(course.total),
-                    "enrolled_max": int(course.enrolled_max),
-                    "week_change": int(course.diff_enr_max - course.diff_enr_min),
-                    "start_date": course.start.strftime("%m/%d/%Y") if course.start is not None else '-',
-                    "end_date": course.end.strftime("%m/%d/%Y") if course.end is not None else '-',
-                    "certificates": int(course.cert_count) if course.cert_count is not None else 0,
-                    "count_graded": int(course.total_passing)
-                }
-            )
-
-        count_pages = (len(output) + COUNT_PER_PAGE) // COUNT_PER_PAGE
+        count_pages = count_courses / COUNT_PER_PAGE
 
         return JsonResponse(
             data={
-                "courses": json.dumps(output),
+                "courses": json.dumps(courses),
                 "total_metrics": json.dumps(self.get_total_metrics()),
-                "count_pages": count_pages,
-                "current_page": int(request.GET.get("page"))
-            }
+                "count_pages": count_pages if count_pages else 1,
+                "current_page": page + 1,
+                "microsites_names": [microsite.site.name for microsite in microsites],
+                "ms_selected": ms_selected,
+            },
+            status=200,
+            content_type='application/json'
         )
